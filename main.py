db6d0ce50a6b726430a4af9b7ab32d5cddee3026
Rabeehrabi <91100522+Rabeehrabi@users.noreply.github.com>
2021-11-12 19:01:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ async def start_handler(_, message: Message):
     try:
         await message.reply_text(
             text="Hello, I am Torrent Search Bot!\n"
-                 "I can search Torrent Magnetic Links from Inline.\n\n"
-                 "Made by @AbirHasan2005",
+                 "I can search Torrent Links from Inline",
             disable_web_page_preview=True,
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(DEFAULT_SEARCH_MARKUP)
